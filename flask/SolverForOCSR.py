c55248495ec8c6ab4890bb99a0d409d07a4ddeae
Awenega <awenega@DESKTOP-BT43K3G>
2023-08-16 11:40:25 +0200

    Fixed OCSR checker

--- flask/SolverForOCSR.py
+++ flask/SolverForOCSR.py
@@ -65,9 +65,7 @@ def is_ocsr(serial_schedule, must_comes_before):
         if serial_schedule.index(transaction_comes_before) > serial_schedule.index(transaction_comes_after):
             return False, f"because T{transaction_comes_before} must come before T{transaction_comes_after}"
     serial_schedule_to_print = [f"T{transaction}" for transaction in serial_schedule]
-    return True, f"<br>Serial Schedule Conflict Equivalent and OCSR: <strong>{''.join(serial_schedule_to_print)}</strong>"
-
-            
+    return True, f"<br>Serial Schedule Conflict Equivalent to S and OCSR: <strong>{''.join(serial_schedule_to_print)}</strong>"
 
 def solveOCSR(schedule):
     order = get_initial_order(schedule)
@@ -81,12 +79,16 @@ def solveOCSR(schedule):
     possible_serial_schedules = get_all_possible_serial_schedule(schedule)        
     if possible_serial_schedules != None:
         for serial_schedule in possible_serial_schedules:
-            return is_ocsr(serial_schedule, must_comes_before)
+            result, msg = is_ocsr(serial_schedule, must_comes_before)
+            if result:
+                return result, msg
+        return result, msg
             
     return False, "because not exist any serial schedule"
 
 # w1(x)r2(x)c2w3(y)c3w1(y)c1 FALSE because T2 comes before T3, but in serial T3T1T2 doesn't come before
 # w1(x)
 # w1(x)r2(x)w1(y)c1c2 TRUE perchè la schedule originale non ha nessun ordine da rispettare, quindi basta avere una serial schedule conflict equivalent?
-# w1(x)r2(x)r2(y)c2c1
+# w1(x)r2(x)r2(y)c2c1 #TRUE T1T2
 # w3(y)c3w1(x)r2(x)c2w1(y)c1 #TRUE T3T1T2 dalle slide
+# r1(x)r2(x)r3(x) #TRUE
